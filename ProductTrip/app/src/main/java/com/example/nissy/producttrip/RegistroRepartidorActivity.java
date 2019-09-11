efe577cba85980302b23c9753cc8aace826c2ff8
IgnacioLuisLuzDeisy <nissy_14@hotmail.com>
2019-09-11 18:11:55 -0500

    primer commit

---
+++ ProductTrip/app/src/main/java/com/example/nissy/producttrip/RegistroRepartidorActivity.java
@@ -0,0 +1,13 @@
+package com.example.nissy.producttrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroRepartidorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_repartidor);
+    }
+}
---
+++ ProductTrip/app/src/main/java/com/example/nissy/producttrip/VistaTiendaActivity.java
@@ -0,0 +1,33 @@
+package com.example.nissy.producttrip;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class VistaTiendaActivity extends AppCompatActivity {
+    private ImageButton add;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_tienda);
+        add = findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent producto = new Intent(VistaTiendaActivity.this, RegistrarProductoActivity.class);//CLIENTE
+                startActivity(producto);
+            }
+        });
+
+    }
+    @Override //REGRESAR ACTIVITY
+    public void onBackPressed() {
+        Intent inten = new Intent();
+        inten.setClass(VistaTiendaActivity.this,LoginActivity.class);
+        startActivity(inten);
+        finish();
+    }
+}
